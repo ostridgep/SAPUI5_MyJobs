8a979352e4e03fa7d3e2e03b8d947371b19c46f3
Paul Ostridge <Paul@ostridge.com>
2015-08-19 17:04:20 +0100

    First Commit

---
+++ SAPUI5_MyJobs/WebContent/myresources/js/MyJobsUtils.js
@@ -0,0 +1,99 @@
+function showMessage(msg){
+	sap.m.MessageToast.show(msg, {
+		type: Error,
+		duration: Number(3000),
+		width: "30em",
+		my: "center center",
+		at: "center center",		
+		autoClose: true,
+
+	});
+}
+function getDate()	{			
+				var currentdate = new Date(); 
+	return zeroFill1(currentdate.getFullYear().toString()) + zeroFill1((currentdate.getMonth()+1).toString() ) + zeroFill1(currentdate.getDate().toString());
+
+}
+function getTime()	{			
+				var currentdate = new Date(); 
+    x1=zeroFill1( currentdate.getHours()).toString();
+          x2=zeroFill1(currentdate.getMinutes()).toString();
+    x3=zeroFill1( currentdate.getSeconds()).toString();
+	return x1+x2+x3;
+
+}
+function getSAPDate()	{			
+				var currentdate = new Date(); 
+	return zeroFill1(currentdate.getDate().toString()) + "."+zeroFill1((currentdate.getMonth()+1).toString() )  + "."+ zeroFill1(currentdate.getFullYear().toString());
+
+}
+function getSAPTime()	{			
+				var currentdate = new Date(); 
+    x1=zeroFill1( currentdate.getHours()).toString();
+          x2=zeroFill1(currentdate.getMinutes()).toString();
+    x3=zeroFill1( currentdate.getSeconds()).toString();
+	return x1 + ":"+x2 + ":"+x3;
+
+}
+function formatDate(dt){
+
+	var formatteddt="";
+	formatteddt=dt.substring(6,8)+"/"+dt.substring(4,6)+"/"+dt.substring(0,4)
+	return formatteddt;
+	}
+function getDateStamp(){
+nowd=getDate();
+nowt=getTime();
+dtstamp=nowd+nowt;
+return dtstamp;
+}
+function formatDateTime(dt){
+
+var formatteddt="";
+formatteddt=dt.substring(6,8)+"-"+dt.substring(4,6)+"-"+dt.substring(0,4)+" "+dt.substring(8,10)+":"+dt.substring(10,12)+":"+dt.substring(12,14);
+return formatteddt;
+}
+function zeroFill1(x){
+    return (x < 10) ? ("0" + x) : x;   
+}
+function isEven(value) {
+return (value%2 == 0);
+} 
+function parseDate(input){
+input=input+"          ";
+//alert (input.substr(0,4)+"/"+input.substr(4,2)+"/"+input.substr(6,2)+" "+input.substr(8,2)+":"+input.substr(10,2));
+ return new Date(input.substr(0,4), input.substr(4,2)-1, input.substr(6,2), // months are 0-based
+                 input.substr(8,2), input.substr(10,2));
+
+}
+function getURLParameters(paramName) 
+{
+        var sURL = window.document.URL.toString();  
+    if (sURL.indexOf("?") > 0)
+    {
+       var arrParams = sURL.split("?");         
+       var arrURLParams = arrParams[1].split("&");      
+       var arrParamNames = new Array(arrURLParams.length);
+       var arrParamValues = new Array(arrURLParams.length);     
+       var i = 0;
+       for (i=0;i<arrURLParams.length;i++)
+       {
+        var sParam =  arrURLParams[i].split("=");
+        arrParamNames[i] = sParam[0];
+        if (sParam[1] != "")
+            arrParamValues[i] = unescape(sParam[1]);
+        else
+            arrParamValues[i] = "No Value";
+       }
+
+       for (i=0;i<arrURLParams.length;i++)
+       {
+                if(arrParamNames[i] == paramName){
+            //alert("Param:"+arrParamValues[i]);
+                return arrParamValues[i];
+             }
+       }
+       return "No Parameters Found";
+    }
+
+}
